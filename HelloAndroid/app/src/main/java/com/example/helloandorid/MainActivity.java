44ce2c7033714f5762f85588535e17d64f66223c
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-10-09 17:02:39 +0300

    Lab1 Homework

---
+++ HelloAndroid/app/src/main/java/com/example/helloandorid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.helloandorid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello, " + editText.getText() + "!");
+            }
+        });
+    }
+}
