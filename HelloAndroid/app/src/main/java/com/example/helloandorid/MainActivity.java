656713323547baa09b303f0195c1bf97ca3aab23
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-10-15 18:42:03 +0300

    Lab2 Homework

--- HelloAndroid/app/src/main/java/com/example/helloandorid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/example/helloandorid/MainActivity.java
@@ -1,29 +1,121 @@
 package com.example.helloandorid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    EditText editText;
+    Button button;
+    Button bShare;
+    Button bSearch;
+    TextView textView;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
-        Button button = (Button) findViewById(R.id.button);
-        TextView textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        bShare = (Button) findViewById(R.id.bShare);
+        bSearch = (Button) findViewById(R.id.bSearch);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.colors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textView.setText("Hello, " + editText.getText() + "!");
+                createDialog();
+            }
+        });
+
+        bShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, editText.getText());
+                intent.setType("text/plain");
+                startActivity(intent);
+            }
+        });
+
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                String url = "https://www.google.com/search?q=" + editText.getText();
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
             }
         });
     }
+
+    private void createDialog(){
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertBuilder.setMessage("Hello, " + editText.getText() + "!");
+        alertBuilder.setPositiveButton("Positive", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "It's good to be positive.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertBuilder.setNegativeButton("Negative", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Why so negative?", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog dialog = alertBuilder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String color = parent.getItemAtPosition(position).toString();
+        switch (color) {
+            case "Red":
+                button.setTextColor(Color.RED);
+                break;
+            case "Blue":
+                button.setTextColor(Color.BLUE);
+                break;
+            case "Green":
+                button.setTextColor(Color.GREEN);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }
